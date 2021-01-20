abd123eaee39af9162bc055f81f5525e49f29084
Aidan Melville <amelville@lcs.on.ca>
2021-01-20 13:08:04 -0500

    Initial Commit

---
+++ Shared/SandwhichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwhichesApp.swift
+//  Shared
+//
+//  Created by Melville, Aidan on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct SandwhichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
